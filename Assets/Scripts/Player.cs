64961b609f4c1fa216b86caef705c2255f9b2eb1
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2018-11-12 18:45:23 +0000

    Add crude walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,12 @@ public class Player : MonoBehaviour {
     private float vertical;
     private float horizontal;
     public bool canMove = true;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
 	
@@ -21,17 +23,25 @@ public class Player : MonoBehaviour {
         if (canMove) {
             if (Input.GetKey("up") || Input.GetKey("w")) {
                 vertical = 1;
+                animator.SetBool("WalkUp",true);
             } else if (Input.GetKey("down") || Input.GetKey("s")) {
                 vertical = -1;
+                animator.SetBool("WalkDown", true);
             } else {
+                animator.SetBool("WalkUp", false);
+                animator.SetBool("WalkDown", false);
                 vertical = 0;
             }
 
             if (Input.GetKey("left") || Input.GetKey("a")) {
                 horizontal = -1;
+                animator.SetBool("WalkLeft", true);
             } else if (Input.GetKey("right") || Input.GetKey("d")) {
                 horizontal = 1;
+                animator.SetBool("WalkRight", true);
             } else {
+                animator.SetBool("WalkLeft", false);
+                animator.SetBool("WalkRight", false);
                 horizontal = 0;
             }
         }
