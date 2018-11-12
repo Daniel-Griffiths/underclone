f28f3551e5ca3081312ad188211ff82056160489
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2018-11-12 23:30:35 +0000

    Add ability to add voices to speach

--- Assets/Scripts/TextWritter.cs
+++ Assets/Scripts/TextWritter.cs
@@ -2,17 +2,20 @@
 using System.Collections;
 using UnityEngine.UI;
 using TMPro;
+using System.Text.RegularExpressions;
 
 public class TextWritter : MonoBehaviour
 {
 
     private float delay = .05f;
-    public string text;
+    private string text;
     private string currentText = "";
+    private AudioSource voice;
 
     // Use this for initialization
     void Start()
     {
+        this.voice = GetComponent<AudioSource>();
         this.text = GetComponent<TextMeshPro>().text;
         StartCoroutine(ShowText());
     }
@@ -20,9 +23,18 @@ public class TextWritter : MonoBehaviour
     IEnumerator ShowText()
     {
         for (int i = 0; i < text.Length; i++) {
-            currentText = text.Substring(0, i+1);
-            this.GetComponent<TextMeshPro>().text = currentText;
+            string currentLetter = text[i].ToString();
+            this.SpeakText(currentLetter);
+            this.GetComponent<TextMeshPro>().text = currentText += currentLetter;
             yield return new WaitForSeconds(delay);
+        }
+    }
+
+    void SpeakText(string currentLetter)
+    {
+        Regex r = new Regex("^[a-zA-Z0-9]*$");
+        if (this.voice && r.IsMatch(currentLetter)) {
+            this.voice.Play();
         }
     }
 }
